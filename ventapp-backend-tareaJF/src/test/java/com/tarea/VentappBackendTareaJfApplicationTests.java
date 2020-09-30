b2540b3c07ca25d2a92405d2ddec8a194a35855b
JoseF82 <jose0818@gmail.com>
2020-09-30 19:45:53 -0300

    ApplicationTest.java  modificado

--- ventapp-backend-tareaJF/src/test/java/com/tarea/VentappBackendTareaJfApplicationTests.java
+++ ventapp-backend-tareaJF/src/test/java/com/tarea/VentappBackendTareaJfApplicationTests.java
@@ -1,13 +1,10 @@
 package com.tarea;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class VentappBackendTareaJfApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+	
 }
