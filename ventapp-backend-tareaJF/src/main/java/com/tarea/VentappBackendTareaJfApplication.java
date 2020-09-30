54cf23bbe35e0c6d470507a45fc76be5e48e776f
JoseF82 <jose0818@gmail.com>
2020-09-30 19:21:30 -0300

    Se subió aplicación spring boot limpia

---
+++ ventapp-backend-tareaJF/src/main/java/com/tarea/VentappBackendTareaJfApplication.java
@@ -0,0 +1,13 @@
+package com.tarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentappBackendTareaJfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentappBackendTareaJfApplication.class, args);
+	}
+
+}
