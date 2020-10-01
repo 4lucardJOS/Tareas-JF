e8058de0420c5834ad01e47eb1d0e5ad68b79967
JoseF82 <jose0818@gmail.com>
2020-10-01 10:54:05 -0300

    Se agrego elementos JPA,estructura tabla,relaciones y get/seters al Venta.java de la capa model

--- ventapp-backend-tareaJF/src/main/java/com/tarea/model/Venta.java
+++ ventapp-backend-tareaJF/src/main/java/com/tarea/model/Venta.java
@@ -1,5 +1,118 @@
 package com.tarea.model;
 
-public class Venta {
+import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.PositiveOrZero;
+import javax.persistence.ForeignKey;
+
+@Entity
+@Table(name = "venta")
+public class Venta {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idVenta;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_vendedor", nullable = false, foreignKey = @ForeignKey(name = "FK_venta_vendedor"))
+	private Vendedor vendedor;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_cliente", nullable = false, foreignKey = @ForeignKey(name = "FK_venta_cliente"))
+	private Cliente cliente;
+	
+	
+	
+	@PositiveOrZero(message = "Costo Venta debe ser igual o mayor que cero")
+	@Column(name = "costo", nullable = false, length = 9)
+	private Integer costo;
+	
+	
+	private LocalDateTime fecha;
+
+
+	public Integer getIdVenta() {
+		return idVenta;
+	}
+
+
+	public void setIdVenta(Integer idVenta) {
+		this.idVenta = idVenta;
+	}
+
+
+	public Vendedor getVendedor() {
+		return vendedor;
+	}
+
+
+	public void setVendedor(Vendedor vendedor) {
+		this.vendedor = vendedor;
+	}
+
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+
+	public Integer getCosto() {
+		return costo;
+	}
+
+
+	public void setCosto(Integer costo) {
+		this.costo = costo;
+	}
+
+
+	public LocalDateTime getFecha() {
+		return fecha;
+	}
+
+
+	public void setFecha(LocalDateTime fecha) {
+		this.fecha = fecha;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idVenta == null) ? 0 : idVenta.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Venta other = (Venta) obj;
+		if (idVenta == null) {
+			if (other.idVenta != null)
+				return false;
+		} else if (!idVenta.equals(other.idVenta))
+			return false;
+		return true;
+	}
+	
 }
