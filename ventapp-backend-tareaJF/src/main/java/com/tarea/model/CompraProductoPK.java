40b8c6cda465f56555208a0c242700da11b89b5e
JoseF82 <jose0818@gmail.com>
2020-10-01 11:00:19 -0300

    Se agrego estructura de relacion,implementacion serializable y hashcode al CompraProductoPK.java de la capa model

--- ventapp-backend-tareaJF/src/main/java/com/tarea/model/CompraProductoPK.java
+++ ventapp-backend-tareaJF/src/main/java/com/tarea/model/CompraProductoPK.java
@@ -1,5 +1,54 @@
 package com.tarea.model;
 
-public class CompraProductoPK {
+import java.io.Serializable;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+public class CompraProductoPK implements Serializable {
+	
+	@ManyToOne
+	@JoinColumn(name = "id_venta", nullable = false)
+	private Venta venta;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_producto", nullable = false)
+	private Producto producto;
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((producto == null) ? 0 : producto.hashCode());
+		result = prime * result + ((venta == null) ? 0 : venta.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CompraProductoPK other = (CompraProductoPK) obj;
+		if (producto == null) {
+			if (other.producto != null)
+				return false;
+		} else if (!producto.equals(other.producto))
+			return false;
+		if (venta == null) {
+			if (other.venta != null)
+				return false;
+		} else if (!venta.equals(other.venta))
+			return false;
+		return true;
+	}
+	
+	
+	
 
 }
