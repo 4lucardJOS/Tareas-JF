464ebb3ec54e27732cdd316e1944360abb6bc94a
JoseF82 <jose0818@gmail.com>
2020-10-01 10:57:18 -0300

    Se agrego elementos JPA,estructura tabla y get/seters al CompraProducto.java de la capa model

--- ventapp-backend-tareaJF/src/main/java/com/tarea/model/CompraProducto.java
+++ ventapp-backend-tareaJF/src/main/java/com/tarea/model/CompraProducto.java
@@ -1,5 +1,35 @@
 package com.tarea.model;
 
-public class CompraProducto {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "compra_producto")
+@IdClass(CompraProductoPK.class)
+
+public class CompraProducto {
+	
+	@Id
+	private Producto producto;
+	
+	@Id
+	private Venta venta;
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	public Venta getVenta() {
+		return venta;
+	}
+
+	public void setVenta(Venta venta) {
+		this.venta = venta;
+	}
 }
