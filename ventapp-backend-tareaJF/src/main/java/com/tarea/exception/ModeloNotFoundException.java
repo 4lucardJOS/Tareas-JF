fc8f952499981fb57a36f974217711dc237a0ba9
JoseF82 <jose0818@gmail.com>
2020-10-20 13:08:41 -0300

    Se agrego en capa Model  Archivo.java  y  definicion capa Exception

---
+++ ventapp-backend-tareaJF/src/main/java/com/tarea/exception/ModeloNotFoundException.java
@@ -0,0 +1,11 @@
+package com.tarea.exception;
+
+
+//@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ModeloNotFoundException extends RuntimeException{
+	
+	public ModeloNotFoundException(String mensaje) {
+		
+		super(mensaje);
+	}
+}
---
+++ ventapp-backend-tareaJF/src/main/java/com/tarea/exception/ResponseExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.tarea.exception;
+
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+public class ResponseExceptionHandler extends ResponseEntityExceptionHandler{//Manejador de excepciones de la respuesta
+	
+	
+
+}
