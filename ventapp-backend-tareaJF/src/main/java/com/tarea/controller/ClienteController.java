5c05a3981a799515862eb406e735b6b5a09db731
JoseF82 <jose0818@gmail.com>
2020-10-23 21:58:28 -0300

    Se creo capa Controller y se definio ClienteController

---
+++ ventapp-backend-tareaJF/src/main/java/com/tarea/controller/ClienteController.java
@@ -0,0 +1,84 @@
+package com.tarea.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.tarea.exception.ModeloNotFoundException;
+import com.tarea.model.Cliente;
+import com.tarea.service.IClienteService;
+
+
+@RestController
+@RequestMapping("/clientes")
+//@CrossOrigin()
+public class ClienteController {
+
+		@Autowired
+		private IClienteService service;
+		
+		@GetMapping
+		public ResponseEntity<List<Cliente>> listar() {
+			List<Cliente> lista = service.listar();
+			return new ResponseEntity<List<Cliente>>(lista, HttpStatus.OK);
+		}
+		
+		@GetMapping("/{id}")
+		public ResponseEntity<Cliente> listarPorId(@PathVariable("id") Integer id) {
+			Cliente cli = service.leerPorId(id);
+			if (cli.getIdCliente() == null) {
+				throw new ModeloNotFoundException("ID NO ENCONTRADO" + id);
+			}
+			return new ResponseEntity<Cliente>(cli, HttpStatus.OK);
+		}
+		
+		//nivel 1
+		@GetMapping("/pageable")
+		public ResponseEntity<Page<Cliente>> listarPageable(Pageable pageable) {
+			Page<Cliente> clientes = service.listarPageable(pageable);
+			return new ResponseEntity<Page<Cliente>>(clientes, HttpStatus.OK);
+		}
+		
+		// nivel 2
+		@PostMapping
+		public ResponseEntity<Object> registrar(@Valid @RequestBody Cliente cliente) {
+			Cliente cli = service.registrar(cliente);
+			//pacientes/4
+			URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cliente.getIdCliente()).toUri();
+			return ResponseEntity.created(location).build();
+		}
+		
+		@PutMapping
+		public ResponseEntity<Cliente> modificar(@Valid @RequestBody Cliente cliente) {
+			Cliente cli = service.modificar(cliente);
+			return new ResponseEntity<Cliente>(cli, HttpStatus.OK);
+		}
+		
+		@DeleteMapping("/{id}")
+		public ResponseEntity<Object> eliminar(@PathVariable("id") Integer id) {
+			Cliente cli = service.leerPorId(id);
+			if (cli.getIdCliente() == null) {
+				throw new ModeloNotFoundException("ID NO ENCONTRADO" + id);
+			}
+			service.eliminar(id);
+			return new ResponseEntity<Object>(HttpStatus.OK);
+		}
+}
+
+
